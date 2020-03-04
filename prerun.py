16f2b6c92ff256d8d71c9f0a1f2473c2c6bd6610
Antonio Villarreal <avillarreal2anl.gov>
2020-03-04 10:56:38 -0500

    initial deltasigma/chopper implementation

---
+++ prerun.py
@@ -0,0 +1,37 @@
+import json
+import os
+import glob
+
+model = 'M001'
+h0 =  0.6167
+worklist = [ ['/cosmo/scratch/projects/MiraTitanU/Grid/{}/L2100/HACC000/analysis/'.format(model), 2100, 3200, h0] ]
+add_to_path_halos = 'Halos/M200/'
+add_to_path_particles = 'Particles/'
+newworklist = []
+
+dir_struct = 0 # handles different structures for how halos are sorted
+
+for workitem in worklist:
+    basepath = workitem[0]
+    path_to_halos = os.path.join(basepath, add_to_path_halos)
+    path_to_particles = os.path.join(basepath, add_to_path_particles)
+    if dir_struct == 0:
+        step_paths = glob.glob(path_to_halos+'STEP*/')
+    if dir_struct == 1:
+        halo_files = glob.glob(path_to_halos+'*sodproperties')
+        step_paths = ['STEP'+halo_file.split('.sodproperties')[0].split('-')[-1]+'/' for halo_file in halo_files]
+
+    for step_path in step_paths:
+        if dir_struct == 0:
+            step_num = step_path.split('STEP')[-1].split('/')[0]
+            halobase = os.path.commonprefix(glob.glob(step_path+'*.sodproperties'))
+        if dir_struct == 1:
+            step_num = step_path.split('STEP')[-1].split('/')[0]
+            halobase = os.path.commonprefix(glob.glob(path_to_halos+'*'+step_num+'*.sodproperties'))
+        stepchoice = os.path.basename(os.path.normpath(step_path))
+        updated_particle_path = os.path.join(path_to_particles,stepchoice)
+        particlebase = os.path.commonprefix(glob.glob(updated_particle_path+'/*.mpicosmo*'))
+        if int(step_num) > 475:
+            newworklist.append([halobase, particlebase, workitem[1], workitem[2], workitem[3]])
+with open('{}_worklist.json'.format(model), 'w') as fp:
+    json.dump(newworklist, fp)
